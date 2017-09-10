e59aac4668357a85171c54d8e1ef730d3f3a715b
Dinesh Prajapati <dprajapati.jpr@gmail.com>
2017-09-10 21:58:55 +0530

    added commentParser lamda function which triggered on s3 bucket objectCreation

--- handler.js
+++ handler.js
@@ -1,66 +1,80 @@
 'use strict';
 const fetch = require('node-fetch');
 const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
-const fs = require('fs');
 const uuid = require('uuid');
 
 
-const s3 = new AWS.S3();
+const s3 = new AWS.S3({'region': 'us-east-1'}); //region should be same as lamda function
 module.exports.comment = (event, context, callback) => {
   console.log("params ",event.pathParameters);
   let params = event.pathParameters;
 
-  const fileName = uuid.v1();
+  const fileName = uuid.v1(); // create file name(random name with uuid)
   const filePath = fileName+".json";
   const comment_api_path = process.env.API_BASE_PATH+ '/comments/' +params.id;
   console.log("filePath ",filePath)
   fetch(comment_api_path)
-  .then(function(response){
-    console.log("response.status",response.status)
-        return response.json();
+  .then(response => {
+    console.log("response.status",response.status);
+    return response.json(); // return json object here
   })
-  .then(function(json){
-      console.log("json => ",json);
-      fs.writeFile(filePath, JSON.stringify(json), 'utf8', (error) => {
-        if (error) {
-              console.log("Error creating jsonFile: ", error);
-        } else {
-          const jsonFileStream= fs.createReadStream(filePath);
-          const params = { 
-              Bucket: process.env.BUCKET,
-              Key: `comments${filePath}`,
-              Body:jsonFileStream
-          };
-          s3.putObject(params, function (err, data) {
-              if (err) {
-                  console.log("Error uploading jsonFile: ", err);
-                } else {
-                  fs.unlink(filePath, function(err) {
-                      if(err && err.code == 'ENOENT') {
-                          // file doens't exist
-                          console.info("File doesn't exist, won't remove it.");
-                      } else if (err) {
-                          // other errors, e.g. maybe we don't have enough permission
-                          console.error("Error occurred while trying to remove file");
-                      } else {
-                          console.info(`removed`);
-                      }
-                  });
-                  console.log("Successfully uploaded jsonFile on S3", data);
-                  const response = {
-                    statusCode: 200,
-                    body: JSON.stringify({
-                      message: 'Go Serverless v1.0! Your function executed successfully!'
-                    }),
-                  };
+  .then(json => {
+      let jsonFileStream = new Buffer.from(JSON.stringify(json)); // JSON Object to Buffer
+      //console.log("jsonFileStream => ",jsonFileStream);
+      const params = { 
+          Bucket: process.env.BUCKET,
+          Key: `comments_${filePath}`,
+          Body: jsonFileStream
+      };
+      s3.putObject(params, function (err, data) {
+          if (err) {
+            console.log("Error uploading jsonFile: ", err);
+            return Promise.reject(new Error(`Failed to upload file on s3 Bucket`));
+          } else {
+            console.log("Successfully uploaded jsonFile on S3", data);
+            const response = {
+              statusCode: 200,
+              body: JSON.stringify({
+                message: `Lamda function getComment executed successfully! comment_${filePath} file uploaded to S3.`
+              }),
+            };
 
-                  callback(null, response);
-                }
-          }); 
-        }
-      });
+            callback(null, response);
+          }
+      }); 
   });
   
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
+
+module.exports.parser = (event, context, callback) => {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+
+    // Get the object from the event and show its content type
+    const bucket = event.Records[0].s3.bucket.name;
+    const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+    const params = {
+        Bucket: bucket,
+        Key: key,
+    };
+    s3.getObject(params, (err, data) => {
+        if (err) {
+            console.log(err);
+            const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
+            console.log(message);
+            callback(message);
+        } else {
+            console.log('CONTENT:', data);
+            console.log('CONTENT TYPE:', data.ContentType);
+            try{
+              let parsedCommentJson = JSON.parse(data.Body.toString()); //Buffer to JSON Object
+              console.log('parsedCommentJson:', parsedCommentJson);
+            } catch(e) {
+               console.error(e);
+            }
+            callback(null, data.ContentType);
+        }
+    });
 };
